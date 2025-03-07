ce7344072a608ab5f4e064752d8b6db1a29fe95a
AlikOsta <167987940+AlikOsta@users.noreply.github.com>
2025-03-07 20:42:52 +0300

    Исправил VisitListView

--- app/views.py
+++ app/views.py
@@ -36,44 +36,46 @@ class MasterDetailView(DetailView):
     context_object_name = 'master'
 
 
-class VisitListView(UserPassesTestMixin, ListView):
+class VisitListView(ListView):
     model = Visit
     template_name = 'app/visit_list.html'
     context_object_name = 'visits'
     paginate_by = 10
 
-    def test_func(self):
-        return self.request.user.is_staff
-
     def get_queryset(self):
         queryset = Visit.objects.all().order_by('-created_at')
         search_query = self.request.GET.get('q', '')
-        status = self.request.GET.get('status', '')
         master_id = self.request.GET.get('master', '')
+        status = self.request.GET.get('status', '')
 
         if search_query:
             queryset = queryset.filter(Q(name__icontains=search_query) | Q(phone__icontains=search_query))
-        if status:
-            queryset = queryset.filter(status=status)
+
         if master_id:
             queryset = queryset.filter(master_id=master_id)
+        
+        if status:
+            queryset = queryset.filter(status=status)
+
         return queryset
-
-
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['status_choices'] = Visit.STATUS_CHOICES
         context['masters'] = Master.objects.all()
+        context['status_choices'] = Visit.STATUS_CHOICES
         context['search_query'] = self.request.GET.get('q', '')
         context['current_status'] = self.request.GET.get('status', '')
         context['current_master'] = self.request.GET.get('master', '')
         return context
     
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_staff:
+            return redirect('app:index')
+        return super().dispatch(request, *args, **kwargs)
+
     def post(self, request):
         visit_id = request.POST.get('visit_id')
         new_status = request.POST.get('status')
         if visit_id and new_status:
             Visit.objects.filter(id=visit_id).update(status=new_status)
         return redirect('app:visit_list')
-
-
