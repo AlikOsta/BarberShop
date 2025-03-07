247be3c4542b960ddd8c787737dc6b4def01374d
AlikOsta <167987940+AlikOsta@users.noreply.github.com>
2025-03-07 18:57:55 +0300

    убрал Меню из вью

--- app/views.py
+++ app/views.py
@@ -3,22 +3,12 @@ from django.views.generic import ListView, DetailView, View, CreateView, Templat
 from .forms import VisitForm
 from .models import Master, Service
 
-MENU = [
-    {'title': 'Главная', 'url': '/', 'active': True},
-    {'title': 'Мастера', 'url': '#masters', 'active': True},
-    {'title': 'Услуги', 'url': '#services', 'active': True},
-    # {'title': 'Отзывы', 'url': '#reviews', 'active': True},
-    # {'title': 'Оставить отзыв', 'url': 'review/create', 'active': True},
-    {'title': 'Записаться на стрижку', 'url': '#orderForms', 'active': True},
-]
-
 
 class ThanksView(TemplateView):
     template_name = 'app/thanks.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['menu'] = MENU
         return context
 
 
